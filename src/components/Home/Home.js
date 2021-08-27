8d7abeb7dd8164c07e6800bb34e1f33633791131
abidnuri <mdabidnuri@gmail.com>
2021-08-27 12:36:50 +0600

    component add

---
+++ src/components/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './../Header/Header';
+import Books from './../Books/Books';
+import Footer from './../Footer/Footer';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Books/>
+            <Footer />
+        </div>
+    );
+};
+
+export default Home;
---
+++ src/components/SingleBook/SingleBook.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SingleBook = () => {
+    return (
+        <div>
+            Single Books
+        </div>
+    );
+};
+
+export default SingleBook;
